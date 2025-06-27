46fd2da3dde4ed98ca3bb15326139a6bda7ddc10
Chris Drake <christopher.a.drake@gmail.com>
2025-06-26 23:35:52 -0400

    Added car model and code to load car model

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import { Engine, Scene } from 'react-babylonjs'
-import { Vector3, ArcRotateCamera } from '@babylonjs/core'
-import { useEffect, useRef } from 'react'
+import { Engine, Model, Scene } from "react-babylonjs"
+import { Vector3, ArcRotateCamera } from "@babylonjs/core"
+import "@babylonjs/loaders"
+import { useEffect, useRef } from "react"
 
 export default function App() {
   const cameraRef = useRef<ArcRotateCamera | null>(null)
@@ -21,16 +22,25 @@ export default function App() {
           target={new Vector3(0, 0, 0)}
           alpha={-Math.PI / 2}
           beta={Math.PI / 3}
-          radius={10}
+          radius={15}
           minZ={0.1}
           wheelPrecision={50}
+          onCreated={(camera) => {
+            camera.attachControl(true)
+          }}
         />
         <hemisphericLight
           name="light"
           direction={new Vector3(0, 1, 0)}
           intensity={0.7}
         />
-        <box name="box" size={2} />
+        <Model
+          name="player_car"
+          rootUrl="/vehicular-assault/assets/models/"
+          sceneFilename="player_car.glb"
+          position={new Vector3(0, 0, 0)}
+          scaling={new Vector3(1, 1, 1)}
+        />
       </Scene>
     </Engine>
   )
