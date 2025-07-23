94b2d3b10ea98b95a3c0b75ea7d955333605dbd2
Chris Drake <christopher.a.drake@gmail.com>
2025-07-22 23:15:38 -0400

    Fix routing and make pylons solid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
-import { Routes, Route } from 'react-router-dom'
-import Home from './pages/Home'
-import Race from './pages/Race'
+import { Routes, Route, Outlet } from "react-router-dom"
+import Home from "./pages/Home"
+import Race from "./pages/Race"
 
 export default function App() {
   return (
-      <Routes>
-        <Route path="/"      element={<Home  />} />
-        <Route path="race"   element={<Race />} />
-      </Routes>
+    <Routes>
+      <Route path="/*" element={<Outlet />}>
+        <Route index element={<Home />} />
+        <Route path="race" element={<Race />} />
+      </Route>
+    </Routes>
   )
 }
